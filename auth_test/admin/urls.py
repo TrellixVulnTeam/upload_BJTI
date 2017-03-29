b8a7cc3acb50fb6deb5d39b6c605dc467cba4645
fape5009 <fabianpersson@hotmail.se>
2017-03-29 15:25:28 +0200

    new changes with authcate

--- auth_test/admin/urls.py
+++ auth_test/admin/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+    url(r'^new_user/', views.new_user, name= 'new_user'),
     url(r'^$', views.index, name='index'),
+
 
 ]
--- auth_test/admin/views.py
+++ auth_test/admin/views.py
@@ -2,38 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from mongoengine.django.auth import *
 from .models import Users
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from mongoengine.queryset import DoesNotExist
 from mongoengine import *
 
 
 from .models import *
 # Create your views here.
-#def index(request):
- #   if request.method == "POST":
-  #      user = User.create_user(request.POST['your_name'], request.POST['pwd'])
-   #     user.save()
-    #    return render(request, "admin_loggin.html")
-    #else:
-     #   return render(request, "admin_loggin.html")
+def new_user(request):
+    print("In new_user")
+    if request.method == "POST":
+        user = User.create_user(request.POST['your_name'], request.POST['pwd'])
+        user.save()
+        return render(request, "new_user.html")
+    else:
+        return render(request, "new_user.html")
 
 def index(request):
+    print("Im in index")
     if request.method == "POST":
         username = request.POST['your_name']
         password = request.POST['pwd']
         user = authenticate(username=username, password=password)
 
-
-        print(user)
-        print("heeeej")
         if user is not None:
             user.backend = 'mongoengine.django.auth.MongoEngineBackend'
             try:
                 request.session['user'] = user
                 login(request, user)
             except User.DoesNotExist:
-                print("sjukt oklart")
-            #request.session['user'] = user
+                print("error does not exist")
             if user.is_authenticated:
                 return render(request, "loggedin.html")
         else:
